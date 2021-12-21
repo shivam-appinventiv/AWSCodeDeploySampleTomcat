3ad3a2df15cbdfaa40ece7032dfbb2cfb7d2dc79
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2021-12-21 22:22:31 +0530

    install.sh tomcat dir changed

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+sudo mv /tmp/LoginWebApp-1.war /usr/bin/apache-tomcat-9.0.56/webapps/LoginWebApp-1.war
 sudo service tomcat restart
