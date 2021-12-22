94c463f83c5b1c458a9b24a1dce8284bd46da3c3
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2021-12-22 10:56:55 +0530

    buildspec and install.sh modified

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo rm -f /usr/bin/apache-tomcat-9.0.56/webapps/LoginWebApp-1.war
+sudo rm -f /usr/bin/apache-tomcat-9.0.56/webapps/LoginWebApp-1
 aws s3 cp s3://java-artifacts/java-project-build/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/bin/apache-tomcat-9.0.56/webapps/LoginWebApp-1.war
 sudo service tomcat restart
